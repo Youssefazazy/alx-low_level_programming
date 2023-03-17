55f0dd9830bf3c66df28ef4a17b5be8824595b9a
youssefazazy <youssefsazazy69@gmail.com>
2023-03-16 18:06:40 -0700

    4th

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,15 @@
-#include<stdio.h>
-#include<unistd.h>
+#include <stdio.h>
+
 /**
  * main - Entry point
  *
- * Return: Always 1 (Success)
+ * Description: A C program that prints with put function
+ *
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-	sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
-	return (1);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
